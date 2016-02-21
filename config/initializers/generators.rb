4d8de184a3cadf3895eb9fa4f6d2004905bd00f1
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-02-21 13:57:41 +0900

    :construction_worker: add initializer fo generators

---
+++ config/initializers/generators.rb
@@ -0,0 +1,9 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.jbuilder false
+  g.template_engine false
+  g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+  g.fixture_replacement :factory_girl
+end
